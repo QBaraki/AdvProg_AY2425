0083ffe488432f03ce53570f389ce46fdefc63bb
QBaraki <buithanhquang2006@gmail.com>
2025-03-28 17:57:47 +0700

    hw(26-03): Added problem 4

---
+++ Homeworks/2025-03-26/src/04.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int BinarySearch(int a[], int n, int l, int r, int target) {
+  if (l > r) {
+    return -1;
+  }
+  int mid = (l + r) / 2;
+  if (a[mid] == target) {
+    return mid + 1;  // only for 1-indexed output
+  }
+  if (a[mid] < target) {
+    return BinarySearch(a, n, mid + 1, r, target);
+  }
+  return BinarySearch(a, n, l, mid - 1, target);
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  int* a = new int[n];
+  for (int i = 0; i < n; ++i) {
+    std::cin >> a[i];
+  }
+  int target;
+  std::cin >> target;
+  std::cout << BinarySearch(a, n, 0, n - 1, target) << '\n';
+  delete[] a;
+}
